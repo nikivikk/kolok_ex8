98bd6d35c2dec33f9d30844284304433ee3f4b31
Vika <Vika@HOME-PC>
2021-11-03 01:33:34 +0300

    Добавьте файлы проекта.

---
+++ kolok_ex8/kolok_ex8.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+int main()
+{
+    int a, R;
+    std::cin >> a >> R;
+    if (a == 2 * R) {
+        std::cout << "YES";
+    }
+    else {
+        std::cout << "NO";
+    }
+}
